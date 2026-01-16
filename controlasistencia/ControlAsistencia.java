5ee1e6c70bc94d1db0538059bc9b16f0b7c21f7e
walter2007-ws <steveandrade2007@gmail.com>
2026-01-16 10:16:35 -0500

    Primer commit

--- controlasistencia/ControlAsistencia.java
+++ controlasistencia/ControlAsistencia.java
@@ -16,6 +16,12 @@ public class ControlAsistencia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+        En el presente se utilizaron dos principios SOLID
+        S- Single Responsability. Cada clase hace unicamente lo que dice
+        L- Liskov Substitution. Las clases pueden reemplazar a sus clases padre, en este caso a las intertfaces.
+        I- Interface Segregation. Todas las interfaces creadas son utilizadas por cada clase.
+        */
         Scanner sc=new Scanner(System.in);
         Asistencia registro=new Asistencia();
         ValidacionMinAsistencia min =new ValidacionMinAsistencia();
@@ -35,3 +41,4 @@ public class ControlAsistencia {
     }
     
 }
+
